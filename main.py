c0254f020ad923f136aaa935bf661d68d8ca5f44
Jenell Weitz <jenellweitz@Jenells-MacBook-Pro.local>
2019-07-11 15:10:50 -0600

    made edits still not 100% correct

--- main.py
+++ main.py
@@ -5,42 +5,62 @@ import os
 app= Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
-def display_signup():
-  return render_template('signup.html')
+username_error = ''
+password_error = ''
+verify_password_error = ''
+username = ''
+password = ''
+verify_password = ''
+email = ''
+email_error = ''
 
-# def is_email():
-#   for char in email:
-#     if "@" in email and if "." in email and if " " not in email:
-#       pass
-#     if email == "":
-#       pass
+@app.route('/')
+def display():
+  return render_template('signup-no-errors.html', username=username, password=password, 
+  verify_password=verify_password, email=email)
 
 @app.route("/", methods = ['POST'])
-
 def get_user_info():
-  username_error = ''
-  password_error = ''
-  verify_password_error = ''
+
   username = request.form['username']
-  username_error = "Please enter a username"
-  return render_template('signup.html', username=username, username_error=username_error)
+  password = request.form['password']
+  verify_password = request.form['verify_password']
+  email = request.form['email']
+
+  if username == '':
+    username_error = "Please enter a username"
+    username = ''
+    return render_template('signup.html', username_error=username_error)
   
-  password = request.form['password']  
-  password_error = "Please enter a password"
-  password_error2 = "Please enter a password between 3 and 20 characters" 
-
-  verify_password = request.form['verify-password']
-  verify_password_error = "Passwords do not match, please try again"
+  if password == '':
+    password_error = "Please enter a password between 3 and 20 characters."
+    if len(password) < 3:
+      password_error = "Please enter a password between 3 and 20 characters."  
+      if len(password) > 20:
+        password_error = "Please enter a password between 3 and 20 characters."
+    return render_template('signup.html', password_error=password_error)
   
-  return render_template('signup.html', password=password, password_error=password_error, 
-  password_error2=password_error2, verify_password=verify_password, verify_password_error=verify_password_error)
-
-
-  if not username_error and not password_error and not verify_password_error:
-    return redirect('/welcome?username={0}'.format(username))
+  if (password != verify_password):
+    verify_password_error = "Passwords do not match, please try again"
+    verify_password = ''
+    return render_template('signup.html', verify_password_error=verify_password_error)
+  
+  if email == '':
+    if not username_error and not password_error and not verify_password_error:
+      return redirect('/welcome?username={0}'.format(username))
   else:
-    return render_template('signup.html', username=username) #email=email
+    return render_template('signup.html', username=username, email=email) 
+  
+  if "@" not in email or "." not in email:
+    email_error = "Please enter a valid email address"
+    if " " in email:
+      email_error = "Please enter a valid email address"
+      if len(email) < 3 or len(email) > 20:
+        email_error = "Please enter a valid email address"
+        email = ''
+    return render_template('signup.html', email_error=email_error)
+
+ 
 
 @app.route("/welcome")
 def welcome():
